3e136eeda4c315ccb64c3278539409bfa32d8174
funnymrmagyar <llevi1462@gmail.com>
2026-03-05 09:35:54 +0200

    lab2megoldas

--- 02.labor/masodik.hs
+++ 02.labor/masodik.hs
@@ -1,3 +1,5 @@
+import System.Win32.Info.Computer (OSVERSIONINFOEX(szCSDVersion))
+import Control.Monad.Trans.Cont (reset)
 -- # 2. labor
 
 -- I. Könyvtárfüggvények használata nélkül, definiáljuk azt a függvényt, amely meghatározza:
@@ -11,17 +13,58 @@ szamJegySzorzat n =mod n 10*szamJegySzorzat(div n 10)
 --     n<0=szjSzorzat (abs n)
 
 -- - egy szám számjegyeinek összegét (2 módszerrel),
--- - egy szám számjegyeinek számát (2 módszerrel),
--- - egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
+szjOsszeg n
+    |n<0=szjOsszeg (abs n)
+    |div n 10==0=mod n 10
+    |otherwise=mod n 10+szjOsszeg (div n 10)
 
+
+szjOsszeg2 n res
+    | n < 0     = szjOsszeg2 (abs n) res
+    | n < 10    = res + n
+    | otherwise = szjOsszeg2 (div n 10) (res + mod n 10)
+-- - egy szám számjegyeinek számát (2 módszerrel),
+szjSzam n
+    | n<0 = szjSzam (abs n)
+    | div n 10==0 = 1
+    | otherwise = 1 + szjSzam (div n 10)
+
+szjSzam2 n = szjSzam2Acc n 0
+  where
+    szjSzam2Acc n res
+        | n < 0     = szjSzam2Acc (abs n) res
+        | n < 10    = res + 1
+        | otherwise = szjSzam2Acc (div n 10) (res + 1)
+-- - egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
 --   ```haskell
 --   > fugv4 577723707 7
 --   35
 --   ```
+fugv4 n szj 
+    | szj > 9 = error "Nem számjegy"
+    | div n 10 == 0 = if mod n 10 == szj then szj else 0
+    | otherwise = if utolsoSzj == szj 
+                  then szj + fugv4 (div n 10) szj 
+                  else fugv4 (div n 10) szj
+    where
+        utolsoSzj = mod n 10
 -- - egy szám páros számjegyeinek számát,
+parosSzj n
+    |n<0=parosSzj(abs n)
+    |n<10=if even n then 1 else 0
+    |otherwise=if even (mod n 10)then 1+parosSzj(div n 10) else parosSzj(div n 10)
 -- - egy szám legnagyobb számjegyét,
+legnagyobbSzj n lg
+    |n<0=legnagyobbSzj (abs n) lg
+    |n<10=max lg n
+    |otherwise=if mod n 10 >lg then legnagyobbSzj(div n 10) (mod n 10) else legnagyobbSzj(div n 10) lg
 -- - egy szám $b$ számrendszerbeli alakjában a $d$-vel egyenlő számjegyek számát (például a $b = 10$-es számrendszerben a $d = 2$-es számjegyek száma),
 --   Példák függvényhívásokra:
+bSzamSzj n b d
+    | n < 0 = error "neg szam"
+    |n < b=if n==d then 1 else 0
+    |otherwise=if mod n b==d then 1+bSzamSzj(div n b)b d else bSzamSzj(div n b) b d
+
 
 --   ```haskell
 --   fugv 7673573 10 7 -> 3
@@ -30,8 +73,14 @@ szamJegySzorzat n =mod n 10*szamJegySzorzat(div n 10)
 --   fugv 345281 16 4 -> 2
 --   ```
 -- - az 1000-ik Fibonacci számot.
-
+fiboN n=fibo 0 1 0 n
+    where
+        fibo _ _ res 0=res
+        fibo a b res n1=fibo b res(b+res) (n1-1)
 -- II. Alkalmazzuk a map függvényt a I.-nél megírt függvényekre.
+
+szjOsszegLs ls=map szjOsszeg ls
+
 
 -- **Megoldott feladatok:**
 
